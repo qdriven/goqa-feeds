5544579bdd9259fcb855801c93af240f242c7647
Patrick <patrickwuke@163.com>
2022-11-21 13:24:56 +0800

    cleanup

--- pkg/scaffold/types.go
+++
@@ -1,7 +0,0 @@
-package scaffold
-
-type ProjectSetting struct {
-	ProjectType        string            `mapstructure: projectType`
-	ProjectTemplateUrl string            `mapstructure: projectTemplateUrl`
-	Addl               map[string]string `mapstructure: addl`
-}
